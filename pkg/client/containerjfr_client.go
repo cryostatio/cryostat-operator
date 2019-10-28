8a22cc4c6f35df67595acfd6eb02c19e91f10717
Elliott Baron <ebaron@redhat.com>
2019-10-28 16:34:54 -0400

    Try listing recordings

--- pkg/client/containerjfr_client.go
+++ pkg/client/containerjfr_client.go
@@ -2,7 +2,7 @@ package flightrecorder
 
 import (
 	"encoding/json"
-	"io"
+	"fmt"
 	"io/ioutil"
 	"net/url"
 	"time"
@@ -19,19 +19,32 @@ type ClientConfig struct {
 
 type ContainerJfrClient struct {
 	config *ClientConfig
+	conn   *websocket.Conn
 }
 
 type CommandMessage struct {
 	Command string   `json:"command"`
-	Args    []string `json:"args,omitempty"`
+	Args    []string `json:"args"`
 }
 
-func Create(config *ClientConfig) *ContainerJfrClient {
-	return &ContainerJfrClient{config}
+func Create(config *ClientConfig) (*ContainerJfrClient, error) {
+	conn, err := newWebSocketConn(config.ServerURL)
+	if err != nil {
+		return nil, err
+	}
+	client := &ContainerJfrClient{config: config, conn: conn}
+	return client, nil
 }
 
-func (client *ContainerJfrClient) Connect() error {
-	urlStr := client.config.ServerURL.String()
+func (client *ContainerJfrClient) Close() error {
+	return client.conn.Close()
+}
+
+Mesages can be out of order, maybe queue, and filter. Look at what container-jfr-web does
+
+func newWebSocketConn(server *url.URL) (*websocket.Conn, error) {
+	time.Sleep(time.Minute) // XXX
+	urlStr := server.String()
 	conn, resp, err := websocket.DefaultDialer.Dial(urlStr, nil)
 	if err != nil {
 		log.Error(err, "failed to connect to command channel", "server", urlStr)
@@ -39,54 +52,122 @@ func (client *ContainerJfrClient) Connect() error {
 			body, _ := ioutil.ReadAll(resp.Body)
 			log.Info("response", "status", resp.Status, "body", string(body))
 		}
-		return err
+		return nil, err
 	}
-	defer conn.Close()
-
-	done := make(chan struct{})
-	gotRead := make(chan struct{}) // XXX
-
-	go func() {
-		defer close(done)
-		for {
-			_, msg, err := conn.ReadMessage() // TODO consider ReadJSON
-			if err != nil {
-				if err != io.EOF {
-					log.Error(err, "could not read message")
-				}
-				return // TODO keep going?
-			}
-			log.Info("read message", "msg", string(msg))
-			close(gotRead)
+	/*conn.SetCloseHandler(func(code int, text string) error {
+		// Attempt to reconnect
+		log.Info("attempting to reconnect to server", "server", urlStr)
+		conn.Close()
+		err = client.newWebSocketConn(server)
+		if err != nil {
+			log.Error(err, "failed to reconnect to server")
+			return err
 		}
-	}()
+		return nil
+	}) */
+	return conn, nil
+}
 
-	pingCmd := &CommandMessage{Command: "ping"}
-	jsonCmd, err := json.Marshal(pingCmd)
+func (client *ContainerJfrClient) connect(host string, port int) error {
+	target := fmt.Sprintf("%s:%d", host, port)
+	connectCmd := &CommandMessage{Command: "connect", Args: []string{target}}
+	jsonCmd, err := json.Marshal(connectCmd)
 	if err != nil {
 		return err
 	}
 	log.Info("sending command", "json", string(jsonCmd))
 
-	err = conn.WriteJSON(pingCmd)
+	err = client.conn.WriteJSON(connectCmd)
 	if err != nil {
-		log.Error(err, "could not write message")
-	}
-
-	<-gotRead // XXX
-
-	err = conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-	if err != nil {
-		log.Error(err, "failed to close connection with server")
+		log.Error(err, "could not write connect message")
 		return err
-	} // TODO how to graceful shutdown
-
-	select {
-	case <-done:
-		log.Info("closed connection with server")
-		return nil
-	case <-time.After(time.Second):
-		log.Info("timed out waiting to close connection with server")
-		return nil
 	}
+
+	_, msg, err := client.conn.ReadMessage()
+	if err != nil {
+		log.Error(err, "could not read connect message")
+		return err
+	}
+	log.Info(string(msg))
+	return nil
+}
+
+func (client *ContainerJfrClient) isConnected() error {
+	isConnectedCmd := &CommandMessage{Command: "is-connected", Args: []string{}}
+	jsonCmd, err := json.Marshal(isConnectedCmd)
+	if err != nil {
+		return err
+	}
+	log.Info("sending command", "json", string(jsonCmd))
+
+	err = client.conn.WriteJSON(isConnectedCmd)
+	if err != nil {
+		log.Error(err, "could not write is-connected message")
+		return err
+	}
+
+	_, msg, err := client.conn.ReadMessage()
+	if err != nil {
+		log.Error(err, "could not read is-connected message")
+		return err
+	}
+	log.Info(string(msg))
+	return nil
+}
+
+func (client *ContainerJfrClient) disconnect() error {
+	disconnectCmd := &CommandMessage{Command: "disconnect", Args: []string{}}
+	jsonCmd, err := json.Marshal(disconnectCmd)
+	if err != nil {
+		return err
+	}
+	log.Info("sending command", "json", string(jsonCmd))
+
+	err = client.conn.WriteJSON(disconnectCmd)
+	if err != nil {
+		log.Error(err, "could not write disconnect message")
+		return err
+	}
+
+	_, msg, err := client.conn.ReadMessage()
+	if err != nil {
+		log.Error(err, "could not read disconnect message")
+		return err
+	}
+	log.Info(string(msg))
+	return nil
+}
+
+func (client *ContainerJfrClient) ListRecordings(host string, port int) error {
+	err := client.isConnected()
+	if err != nil {
+		return err
+	}
+
+	err = client.connect(host, port)
+	if err != nil {
+		return err
+	}
+	defer client.disconnect()
+
+	listCmd := &CommandMessage{Command: "list", Args: []string{}}
+	jsonCmd, err := json.Marshal(listCmd)
+	if err != nil {
+		return err
+	}
+	log.Info("sending command", "json", string(jsonCmd))
+
+	err = client.conn.WriteJSON(listCmd)
+	if err != nil {
+		log.Error(err, "could not write list message")
+		return err
+	}
+
+	_, msg, err := client.conn.ReadMessage()
+	if err != nil {
+		log.Error(err, "could not read list message")
+		return err
+	}
+	log.Info(string(msg))
+	return nil
 }
--- pkg/controller/flightrecorder/flightrecorder_controller.go
+++ pkg/controller/flightrecorder/flightrecorder_controller.go
@@ -2,12 +2,13 @@ package flightrecorder
 
 import (
 	"context"
+	"fmt"
 	"net/url"
 
 	rhjmcv1alpha1 "github.com/rh-jmc-team/container-jfr-operator/pkg/apis/rhjmc/v1alpha1"
 	jfrclient "github.com/rh-jmc-team/container-jfr-operator/pkg/client"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -61,8 +62,9 @@ var _ reconcile.Reconciler = &ReconcileFlightRecorder{}
 type ReconcileFlightRecorder struct {
 	// This client, initialized using mgr.Client() above, is a split client
 	// that reads objects from the cache and writes to the apiserver
-	client client.Client
-	scheme *runtime.Scheme
+	client    client.Client
+	scheme    *runtime.Scheme
+	jfrClient *jfrclient.ContainerJfrClient
 }
 
 // Reconcile reads that state of the cluster for a FlightRecorder object and makes changes based on the state read
@@ -77,32 +79,20 @@ func (r *ReconcileFlightRecorder) Reconcile(request reconcile.Request) (reconcil
 	reqLogger := log.WithValues("Request.Namespace", request.Namespace, "Request.Name", request.Name)
 	reqLogger.Info("Reconciling FlightRecorder")
 
-	commandSvc := &corev1.Service{}
-	commandSvcName := "containerjfr-command"                                                                       // TODO make const or get from ContainerJFR
-	err := r.client.Get(ctx, types.NamespacedName{Namespace: request.Namespace, Name: commandSvcName}, commandSvc) // TODO what if it's a different namespace
-	if err != nil {
-		// Need service in order to reconcile anything, requeue until it appears
-		return reconcile.Result{}, err
-	}
-
-	clusterIP := commandSvc.Spec.ClusterIP
-	if clusterIP == "" || clusterIP == corev1.ClusterIPNone {
-		// TODO log error
-	}
-	commandURL := &url.URL{Scheme: "ws", Host: clusterIP + ":9090" /* FIXME make nicer */, Path: "command"}
-	config := &jfrclient.ClientConfig{ServerURL: commandURL}
-	jfrClient := jfrclient.Create(config)
-	err = jfrClient.Connect()
-	if err != nil {
-		log.Error(err, "failed to connect to command server")
-		return reconcile.Result{}, err
+	if r.jfrClient == nil {
+		jfrClient, err := r.connectToContainerJFR(ctx, request.Namespace)
+		if err != nil {
+			// Need service in order to reconcile anything, requeue until it appears
+			return reconcile.Result{}, err
+		}
+		r.jfrClient = jfrClient
 	}
 
 	// Fetch the FlightRecorder instance
 	instance := &rhjmcv1alpha1.FlightRecorder{}
-	err = r.client.Get(ctx, request.NamespacedName, instance)
+	err := r.client.Get(ctx, request.NamespacedName, instance)
 	if err != nil {
-		if errors.IsNotFound(err) {
+		if kerrors.IsNotFound(err) {
 			// Request object not found, could have been deleted after reconcile request.
 			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
 			// Return and don't requeue
@@ -112,6 +102,55 @@ func (r *ReconcileFlightRecorder) Reconcile(request reconcile.Request) (reconcil
 		return reconcile.Result{}, err
 	}
 
+	targetRef := instance.Status.Target
+	targetSvc := &corev1.Service{}
+	err = r.client.Get(ctx, types.NamespacedName{Namespace: targetRef.Namespace, Name: targetRef.Name}, targetSvc)
+	if err != nil {
+		return reconcile.Result{}, err // TODO should we requeue?
+	}
+
+	clusterIP, err := getClusterIP(targetSvc)
+	if err != nil {
+		return reconcile.Result{}, err // TODO should we requeue?
+	}
+	err = r.jfrClient.ListRecordings(*clusterIP, 9091) // FIXME hardcoded port
+	if err != nil {
+		log.Error(err, "failed to connect to command server")
+		r.jfrClient.Close()
+		r.jfrClient = nil
+		return reconcile.Result{}, err
+	}
+
 	reqLogger.Info("Found FlightRecorder", "Namespace", instance.Namespace, "Name", instance.Name)
 	return reconcile.Result{}, nil
+}
+
+func (r *ReconcileFlightRecorder) connectToContainerJFR(ctx context.Context, namespace string) (*jfrclient.ContainerJfrClient, error) {
+	commandSvc := &corev1.Service{}
+	commandSvcName := "containerjfr-command"                                                               // TODO make const or get from ContainerJFR
+	err := r.client.Get(ctx, types.NamespacedName{Namespace: namespace, Name: commandSvcName}, commandSvc) // TODO what if it's a different namespace
+	if err != nil {
+		return nil, err
+	}
+
+	clusterIP, err := getClusterIP(commandSvc)
+	if err != nil {
+		return nil, err
+	}
+	host := fmt.Sprintf("%s:%d", *clusterIP, 9090) // FIXME hardcoded port
+	commandURL := &url.URL{Scheme: "ws", Host: host, Path: "command"}
+	config := &jfrclient.ClientConfig{ServerURL: commandURL}
+	jfrClient, err := jfrclient.Create(config)
+	if err != nil {
+		return nil, err
+	}
+	return jfrClient, nil
+}
+
+func getClusterIP(svc *corev1.Service) (*string, error) {
+	clusterIP := svc.Spec.ClusterIP
+	if clusterIP == "" || clusterIP == corev1.ClusterIPNone {
+		return nil, fmt.Errorf("ClusterIP unavailable for %s", svc.Name)
+	}
+	return &clusterIP, nil
 }
