9687cf0458ac00febd4993ef5a4c154defcd4106
Andrew Azores <me@andrewazor.es>
2019-11-06 15:32:17 -0500

    Update container-jfr to v0.4.12 and operator to v0.1.6

--- pkg/controller/containerjfr/resource_definitions/resource_definitions.go
+++ pkg/controller/containerjfr/resource_definitions/resource_definitions.go
@@ -74,7 +74,7 @@ func NewPodForCR(cr *rhjmcv1alpha1.ContainerJFR, specs *ServiceSpecs) *corev1.Po
 func NewCoreContainer(cr *rhjmcv1alpha1.ContainerJFR, specs *ServiceSpecs) corev1.Container {
 	return corev1.Container{
 		Name:  cr.Name,
-		Image: "quay.io/rh-jmc-team/container-jfr:0.4.11",
+		Image: "quay.io/rh-jmc-team/container-jfr:0.4.12",
 		VolumeMounts: []corev1.VolumeMount{
 			{
 				Name:      cr.Name,
