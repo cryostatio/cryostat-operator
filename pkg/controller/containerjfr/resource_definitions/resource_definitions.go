43454659f217e3ccd31306925f50681b79a54017
Andrew Azores <me@andrewazor.es>
2019-11-21 15:32:18 -0500

    Update container-jfr image version

--- pkg/controller/containerjfr/resource_definitions/resource_definitions.go
+++ pkg/controller/containerjfr/resource_definitions/resource_definitions.go
@@ -80,7 +80,7 @@ func NewPodForCR(cr *rhjmcv1alpha1.ContainerJFR, specs *ServiceSpecs) *corev1.Po
 }
 
 func NewCoreContainer(cr *rhjmcv1alpha1.ContainerJFR, specs *ServiceSpecs) corev1.Container {
-	imageTag := "quay.io/rh-jmc-team/container-jfr:0.4.14"
+	imageTag := "quay.io/rh-jmc-team/container-jfr:0.5.2"
 	if cr.Spec.Minimal {
 		imageTag += "-minimal"
 	}
