9c44214b471c12929cec665c07ad72b72c9ec054
Andrew Azores <aazores@redhat.com>
2020-11-24 10:17:47 -0500

    Prefix variables with GRAFANA_

--- images/grafana/build.sh
+++ images/grafana/build.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-if [ -z "$IMAGE" ]; then
-    IMAGE="quay.io/rh-jmc-team/container-jfr-grafana-dashboard"
+if [ -z "$GRAFANA_IMAGE" ]; then
+    GRAFANA_IMAGE="quay.io/rh-jmc-team/container-jfr-grafana-dashboard"
 fi
 
-if [ -z "$TAG" ]; then
-    TAG="0.1.0"
+if [ -z "$GRAFANA_TAG" ]; then
+    GRAFANA_TAG="0.1.0"
 fi
 
 if [ -z "$BUILDER" ]; then
     BUILDER="podman"
 fi
 
-$BUILDER build -t $IMAGE:$TAG -f "$(dirname $0)"/Containerfile
-$BUILDER tag $IMAGE:$TAG $IMAGE:latest
+$BUILDER build -t $GRAFANA_IMAGE:$GRAFANA_TAG -f "$(dirname $0)"/Containerfile
+$BUILDER tag $GRAFANA_IMAGE:$GRAFANA_TAG $GRAFANA_IMAGE:latest
