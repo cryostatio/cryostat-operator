c025b26c9b65aada677c07f0350502148446a6f3
Andrew Azores <aazores@redhat.com>
2021-02-05 17:30:58 -0500

    Port Recording Controller and API

--- main.go
+++ main.go
@@ -86,6 +86,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "ContainerJFR")
 		os.Exit(1)
 	}
+	if err = (&controllers.RecordingReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("Recording"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Recording")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("health", healthz.Ping); err != nil {
