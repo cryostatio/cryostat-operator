aaa33d6da065304975b0c7077f7eab45b1f84800
Andrew Azores <me@andrewazor.es>
2021-02-08 17:53:58 -0500

    Port FlightRecorder Controller and API

--- main.go
+++ main.go
@@ -94,6 +94,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Recording")
 		os.Exit(1)
 	}
+	if err = (&controllers.FlightRecorderReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("FlightRecorder"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "FlightRecorder")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("health", healthz.Ping); err != nil {
