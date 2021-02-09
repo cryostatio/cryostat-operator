dfb3d8aa78aadd7515563aefb3b255a7190b3f82
Andrew Azores <me@andrewazor.es>
2021-02-09 17:06:31 -0500

    Add some RBAC markers

--- controllers/recording_controller.go
+++ controllers/recording_controller.go
@@ -80,10 +80,18 @@ type RecordingReconciler struct {
 // Name used for Finalizer that handles Container JFR recording deletion
 const recordingFinalizer = "recording.finalizer.rhjmc.redhat.com"
 
+// +kubebuilder:rbac:groups="",resources=pods;services;services/finalizers;routes;endpoints;persistentvolumeclaims;events;configmaps;secrets,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=route.openshift.io,resources=routes;routes/custom-host,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=apps,resources=deployments;daemonsets;replicasets;statefulsets,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=monitoring.coreos.com,resources=servicemonitors,verbs=get;create
+// +kubebuilder:rbac:groups=apps,resources=deployments/finalizers,resourceNames=container-jfr-operator,verbs=update
+// +kubebuilder:rbac:groups="",resources=pods,verbs=get
+// +kubebuilder:rbac:groups=apps,resources=replicasets,verbs=get
+// +kubebuilder:rbac:groups=rhjmc.redhat.com,resources=*;flightrecorders;recordings;containerjfrs,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=cert-manager.io,resources=issuers;certificates,verbs=create;get;list;update;watch
 // +kubebuilder:rbac:groups=rhjmc.redhat.com,resources=recordings,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=rhjmc.redhat.com,resources=recordings/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=rhjmc.redhat.com,resources=recordings/finalizers,verbs=update
-
 func (r *RecordingReconciler) Reconcile(ctx context.Context, request ctrl.Request) (ctrl.Result, error) {
 	reqLogger := r.Log.WithValues("Request.Namespace", request.Namespace, "Request.Name", request.Name)
 	reqLogger.Info("Reconciling Recording")
