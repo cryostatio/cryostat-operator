8c7b8af9f0d627b928bde6c26edfcd175b4d1972
Elliott Baron <ebaron@redhat.com>
2021-02-11 15:55:36 -0500

    Fix error where datasource input can't be deleted

--- images/grafana/cjfr-run.sh
+++ images/grafana/cjfr-run.sh
@@ -3,10 +3,11 @@
 # Base image entrypoint
 RUN_GRAFANA="/run.sh"
 
+# Datasource config
+DATASOURCE_YAML="/etc/grafana/provisioning/datasources/datasource.yaml"
+
 # Substitute value of JFR_DATASOURCE_URL environment variable into datasource.yaml
-# and move it to the expected provisioning directory
-sed -e "s|JFR_DATASOURCE_URL|${JFR_DATASOURCE_URL}|g" /tmp/datasource.yaml.in > /etc/grafana/provisioning/datasources/datasource.yaml
-rm -f /tmp/datasource.yaml.in
+sed -i "s|JFR_DATASOURCE_URL|${JFR_DATASOURCE_URL}|g" "${DATASOURCE_YAML}"
 
 # Run base entrypoint with any additional command line args
 exec "${RUN_GRAFANA}" "$@"
