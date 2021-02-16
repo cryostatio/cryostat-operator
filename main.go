8f38b4e7c86c26b88876bdc2668bb6ecbff29851
Andrew Azores <me@andrewazor.es>
2021-02-16 16:19:56 -0500

    Port Endpoints controller

--- main.go
+++ main.go
@@ -128,6 +128,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "FlightRecorder")
 		os.Exit(1)
 	}
+	if err = (&controllers.EndpointsReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("Endpoints"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Endpoints")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("health", healthz.Ping); err != nil {
