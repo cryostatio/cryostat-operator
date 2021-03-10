83e2678b0030f8f45395199e13d0619336899693
Andrew Azores <me@andrewazor.es>
2021-03-10 08:42:58 -0500

    Remove unused import

--- test/resources.go
+++ test/resources.go
@@ -47,7 +47,6 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/kubernetes/scheme"
 )
 
