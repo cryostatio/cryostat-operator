5e5ba0f89f6489d070d2af2fb51492b853cae4b2
Elliott Baron <ebaron@redhat.com>
2021-07-14 17:06:21 -0400

    Update image versions

--- internal/controllers/common/resource_definitions/imagetag_generated.go
+++ internal/controllers/common/resource_definitions/imagetag_generated.go
@@ -2,7 +2,7 @@
 package resource_definitions
 
 // Default image tag for the core application image
-const DefaultCoreImageTag = "quay.io/cryostat/cryostat:1.0.0"
+const DefaultCoreImageTag = "quay.io/cryostat/cryostat:2.0.0-SNAPSHOT"
 
 // Default image tag for the JFR datasource image
 const DefaultDatasourceImageTag = "quay.io/cryostat/jfr-datasource:1.0.0"
