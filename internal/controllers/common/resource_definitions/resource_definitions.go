25cf21d1a16257b58d201b81c2d30e40ee425370
Janelle Law <jalaw@redhat.com>
2021-11-05 14:49:31 -0400

    Set redirect uri dynamically

--- internal/controllers/common/resource_definitions/resource_definitions.go
+++ internal/controllers/common/resource_definitions/resource_definitions.go
@@ -38,6 +38,7 @@ package resource_definitions
 
 import (
 	"crypto/sha256"
+	"encoding/json"
 	"fmt"
 	"math/rand"
 	"net/url"
@@ -46,6 +47,7 @@ import (
 
 	operatorv1beta1 "github.com/cryostatio/cryostat-operator/api/v1beta1"
 	consolev1 "github.com/openshift/api/console/v1"
+	oauthv1 "github.com/openshift/api/oauth/v1"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
@@ -827,7 +829,19 @@ func NewKeystoreSecretForCR(cr *operatorv1beta1.Cryostat) *corev1.Secret {
 	}
 }
 
-func NewServiceAccountForCR(cr *operatorv1beta1.Cryostat) *corev1.ServiceAccount {
+func NewServiceAccountForCR(cr *operatorv1beta1.Cryostat) (*corev1.ServiceAccount, error) {
+	OAuthRedirectReference := &oauthv1.OAuthRedirectReference{
+		Reference: oauthv1.RedirectReference{
+			Kind: "Route",
+			Name: cr.Name,
+		},
+	}
+
+	ref, err := json.Marshal(OAuthRedirectReference)
+	if err != nil {
+		return nil, err
+	}
+
 	sa := &corev1.ServiceAccount{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      cr.Name,
@@ -836,11 +850,12 @@ func NewServiceAccountForCR(cr *operatorv1beta1.Cryostat) *corev1.ServiceAccount
 				"app": "cryostat",
 			},
 			Annotations: map[string]string{
-				"serviceaccounts.openshift.io/oauth-redirecturi.route": cr.Status.ApplicationURL,
+				"serviceaccounts.openshift.io/oauth-redirecturi.route":       "https://",
+				"serviceaccounts.openshift.io/oauth-redirectreference.route": string(ref),
 			},
 		},
 	}
-	return sa
+	return sa, nil
 }
 
 func NewRoleForCR(cr *operatorv1beta1.Cryostat) *rbacv1.Role {
