62220feb9b2d8bf5dc97a1d22225587bf9b0918f
Janelle Law <jalaw@redhat.com>
2021-12-07 12:48:16 -0500

    Add tree API get permissions

--- internal/controllers/cryostat_controller.go
+++ internal/controllers/cryostat_controller.go
@@ -108,7 +108,9 @@ var supGroupRegexp = regexp.MustCompile(`^\d+`)
 // +kubebuilder:rbac:groups=authentication.k8s.io,resources=tokenreviews,verbs=create
 // +kubebuilder:rbac:groups="",resources=namespaces,verbs=get;list;watch
 // +kubebuilder:rbac:groups="",resources=pods,verbs=create;get
+// +kubebuilder:rbac:groups="",resources=replicationcontrollers,endpoints,verbs=get
 // +kubebuilder:rbac:groups=apps,resources=deployments,verbs=create;get
+// +kubebuilder:rbac:groups=apps,resources=daemonsets;replicasets;statefulsets,verbs=get
 // +kubebuilder:rbac:groups=operator.cryostat.io,resources=flightrecorders,verbs=create;delete;get;patch
 // +kubebuilder:rbac:groups=operator.cryostat.io,resources=cryostats,verbs=create;delete;get
 // +kubebuilder:rbac:groups=operator.cryostat.io,resources=recordings,verbs=get;create;patch;delete
