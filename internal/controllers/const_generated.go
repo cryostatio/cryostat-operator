c8ecb793a0beb4df8a7e46c561f02cd5ba3269f6
Andrew Azores <me@andrewazor.es>
2022-10-28 11:46:52 -0400

    reset tags

--- internal/controllers/const_generated.go
+++ internal/controllers/const_generated.go
@@ -5,13 +5,13 @@ package controllers
 const AppName = "Cryostat"
 
 // Default image tag for the core application image
-const DefaultCoreImageTag = "quay.io/cryostat/cryostat:2.2"
+const DefaultCoreImageTag = "quay.io/cryostat/cryostat:2.2.0-beta1"
 
 // Default image tag for the JFR datasource image
-const DefaultDatasourceImageTag = "quay.io/cryostat/jfr-datasource:2.2"
+const DefaultDatasourceImageTag = "quay.io/cryostat/jfr-datasource:cryostat-v2.2"
 
 // Default image tag for the Grafana dashboard image
-const DefaultGrafanaImageTag = "quay.io/cryostat/cryostat-grafana-dashboard:v2.2"
+const DefaultGrafanaImageTag = "quay.io/cryostat/cryostat-grafana-dashboard:cryostat-v2.2"
 
 // Default image tag for the Grafana dashboard image
-const DefaultReportsImageTag = "quay.io/cryostat/cryostat-reports:2.2"
+const DefaultReportsImageTag = "quay.io/cryostat/cryostat-reports:cryostat-v2.2"
