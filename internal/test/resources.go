47fa7b48337468f41e05d3fb6561521a4ae4b4bd
Elliott Baron <ebaron@redhat.com>
2023-04-27 15:11:05 -0400

    fix(deploy): lower default Grafana CPU request (#566)
    Signed-off-by: Elliott Baron <ebaron@redhat.com>

--- internal/test/resources.go
+++ internal/test/resources.go
@@ -2695,7 +2695,7 @@ func (r *TestResources) NewDatasourceContainerResource(cr *model.CryostatInstanc
 func newGrafanaContainerDefaultResource() *corev1.ResourceRequirements {
 	return &corev1.ResourceRequirements{
 		Requests: corev1.ResourceList{
-			corev1.ResourceCPU:    resource.MustParse("1000m"),
+			corev1.ResourceCPU:    resource.MustParse("100m"),
 			corev1.ResourceMemory: resource.MustParse("256Mi"),
 		},
 	}
