2b88afc9d6cefbd6a80a22b56554a1ce6142e3bd
Ming Wang <miwan@redhat.com>
2023-09-08 14:20:02 -0400

    update versions for 2.3.1 release

--- internal/controllers/const_generated.go
+++ internal/controllers/const_generated.go
@@ -5,13 +5,13 @@ package controllers
 const AppName = "Cryostat"
 
 // Default image tag for the core application image
-const DefaultCoreImageTag = "quay.io/cryostat/cryostat:2.3.1-snapshot"
+const DefaultCoreImageTag = "quay.io/cryostat/cryostat:2.3.1"
 
 // Default image tag for the JFR datasource image
-const DefaultDatasourceImageTag = "quay.io/cryostat/jfr-datasource:2.3.1-snapshot"
+const DefaultDatasourceImageTag = "quay.io/cryostat/jfr-datasource:2.3.1"
 
 // Default image tag for the Grafana dashboard image
 const DefaultGrafanaImageTag = "quay.io/cryostat/cryostat-grafana-dashboard:cryostat-v2.3"
 
 // Default image tag for the Grafana dashboard image
-const DefaultReportsImageTag = "quay.io/cryostat/cryostat-reports:2.3.1-snapshot"
+const DefaultReportsImageTag = "quay.io/cryostat/cryostat-reports:2.3.1"
