2173b0596a261b41896d111ef202c68a15376fd9
Ming Wang <miwan@redhat.com>
2024-02-27 10:46:39 -0500

    custom target discovery

--- internal/test/scorecard/types.go
+++ internal/test/scorecard/types.go
@@ -113,6 +113,20 @@ type Target struct {
 	Alias      string `json:"alias,omitempty"`
 }
 
+type TargetCreateOptions struct {
+	ConnectUrl string `json:"connectUrl"`
+	Alias      string `json:"alias"`
+}
+
+func (opts *TargetCreateOptions) ToFormData() string {
+	formData := &url.Values{}
+
+	formData.Add("connectUrl", opts.ConnectUrl)
+	formData.Add("alias", opts.Alias)
+
+	return formData.Encode()
+}
+
 type GraphQLQuery struct {
 	Query     string            `json:"query"`
 	Variables map[string]string `json:"variables,omitempty"`
